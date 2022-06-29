485683d2e27391fe0aa190e6b890f5e7b5787d6e
Aidin53 <aidin53.110@gmail.com>
2022-06-28 23:44:13 -0700

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// NOTE: edit database url in .env file
+
+export default function Home() {
+    return (
+        <div className="h-screen flex flex-col items-center justify-center">
+            <h1 className="text-2xl text-slate-900 font-semibold">Nextjs Typescript with Tailwindcss and Prisma</h1>
+        </div>
+    );
+}
